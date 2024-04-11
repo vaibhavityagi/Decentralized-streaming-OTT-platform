ca4606822ef37c5ad8ba502f170569b63f2e25c9
vaibhavityagi <vaibhavityagi5@gmail.com>
2024-04-11 20:12:42 +0530

    imported footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import "./App.css";
 import Test from "./pages/Test";
+import Footer from "./components/Footer";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);

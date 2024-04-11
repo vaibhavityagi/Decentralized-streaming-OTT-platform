67170563e527806d5c29b275c5f97de5a455248e
unknown <jhashivangisingh03@gmail.com>
2024-04-11 19:04:33 +0530

    App css content modified

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line react/prop-types
 export default function Button({ text, color, border }) {
   console.log("hit")
-  return <button style={{ border: border, cursor: "pointer", backgroundColor: color, color: "white", padding: "10px 20px", borderRadius: "5px", marginInlineEnd: "1rem" }}>
+  return <button style={{ border: border, cursor: "pointer", backgroundColor: color, color: "white", padding: "10px 15px", borderRadius: "5px", marginInlineEnd: "1rem" }}>
     {text}</button>;
 }
--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -2,7 +2,7 @@ import Card from "../components/Card";
 
 export default function Test() {
     return <div>
-        <Card type="plan" heading="Basic Plan" body="I am body"
-            icon="im icon" price={99}></Card>
+        <Card type="plan" heading="Basic Plan" body="Enjoy an extensive library of movies and shows, featuring a range of content, including recently released titles."
+            icon="im icon" price={99}>/month</Card>
     </div>
 }

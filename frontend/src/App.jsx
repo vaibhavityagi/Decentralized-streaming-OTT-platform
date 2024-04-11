6c615d12d18e03a1ba7a035db56e145f4c580ab3
vaibhavityagi <vaibhavityagi5@gmail.com>
2024-04-11 22:13:44 +0530

    sign up complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
+import "./App.css";
 import { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import "./App.css";
-import Test from "./pages/Test";
 import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,9 +29,10 @@ function App() {
             element={<Signup setIsLoggedIn={setIsLoggedIn} />}
           ></Route>
 
-          <Route path="/test" element={<Test />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
         </Routes>
         <Footer />
+        <ToastContainer />
       </BrowserRouter>
     </>
   );

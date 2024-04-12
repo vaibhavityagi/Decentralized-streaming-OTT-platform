4f12295cb1cec327074dfd42e6095df3f7311a05
unknown <jhashivangisingh03@gmail.com>
2024-04-12 15:42:51 +0530

    faq flex

--- frontend/src/components/Faqs.jsx
+++ frontend/src/components/Faqs.jsx
@@ -1,9 +1,10 @@
 import Faq from "./Faq";
+import "../App.css";
 
 export default function Faqs() {
   return (
-    <div>
-      <Faq
+    <div className="f">
+      <div><Faq
         question="Why are we better than other streaming platforms?"
         num={1}
         open={false}
@@ -11,31 +12,31 @@ export default function Faqs() {
         ***** is a streaming platform where you can watch movies and web series
         as per your convenience without buying the monthy or yearly
         subscription. Just subscribe for a day or an hour!
-      </Faq>
-      <Faq question="What content is available on *****" num={2} open={false}>
+      </Faq></div>
+      <div><Faq question="What content is available on *****" num={2} open={false}>
         You can watch anything and everything on **** starting only at ***
-      </Faq>
-      <Faq
+      </Faq></div>
+      <div><Faq
         question="Is there any scheme of refund in *****"
         num={3}
         open={false}
       >
         Once you have bought the tokens, they cannot be refunded. Although you
         can use them sitewise.
-      </Faq>
-      <Faq question="What are the subscription plans?" num={4} open={false}>
+      </Faq></div>
+      <div><Faq question="What are the subscription plans?" num={4} open={false}>
         **********************
-      </Faq>
-      <Faq question="How do I resolve my queries?" num={5} open={false}>
+      </Faq></div>
+      <div><Faq question="How do I resolve my queries?" num={5} open={false}>
         You can send a mail to us at ****@gmail.com
-      </Faq>
-      <Faq
+      </Faq></div>
+      <div><Faq
         question="How long it take to resolve the issue?"
         num={6}
         open={false}
       >
         Sometimes It may take 24 working hours to resolve the issue .
-      </Faq>
+      </Faq></div>
     </div>
   );
 }

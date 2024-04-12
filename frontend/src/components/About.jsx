2a785ee263dfbf81dc6c1b805b770d58d5c5a204
Ashutosh Mishra <ashuu.pvt@gmail.com>
2024-04-12 20:35:48 +0530

    about us done

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,8 +1,11 @@
+import "../css/about.css";
 export default function About({ children }) {
   return (
-    <div>
-      <h2>About Us</h2>
-      <div>{children}</div>
+    <div className="container">
+    <div className="head">
+    <h2>About Us</h2>
+    </div>
+      <div className="child">{children}</div>
     </div>
   );
 }

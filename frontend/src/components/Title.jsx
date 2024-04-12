5ec8e2ddd1483a356fae581e6119602149b1e5e3
Ashutosh Mishra <ashuu.pvt@gmail.com>
2024-04-12 17:40:07 +0530

    landingpageupdated

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -1,11 +1,19 @@
+import "../css/landingpage.css";
 import VideoButton from "./VideoButton";
 
 export default function Title() {
   return (
-    <div>
-      <h2>The best streaming experience</h2>
+    <div className="container-0">
+    <div className="box-0">
+      <p>The Best Streaming Experience</p>
+    </div>
+    <div className="box-1">
       <p>Now only at ₹99/-</p>
-      <VideoButton text="Start Watching Now" color="blue" border="" />
+    </div>
+    <div className="box-2">
+      <VideoButton text="Start Watching Now" color="#00A3FF" border="" />
+    </div>
+      
     </div>
   );
 }

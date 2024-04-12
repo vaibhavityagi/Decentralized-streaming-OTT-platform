73c56185fc876c25e136cd08732847f746bcee51
vaibhavityagi <vaibhavityagi5@gmail.com>
2024-04-12 20:11:26 +0530

    movie card component ready

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -3,16 +3,16 @@ import { FaRegEye } from "react-icons/fa";
 
 export default function MovieCard({ duration, ratings, image }) {
   return (
-    <div>
-      <img src={image} alt="movie" />
-      <div className="flex gap-1">
-        <div className="flex gap-1 items-center">
+    <div className="bg-richblack-1000 p-2 w-48 rounded m-5 px-3">
+      <img src={image} alt="movie" className="rounded my-1" />
+      <div className="flex justify-between mt-2">
+        <div className="flex gap-1 items-center my-1 rounded-xl bg-richblack-300 p-1 px-1.5">
           <div>
             <FaClock />
           </div>
           <div>{duration}</div>
         </div>
-        <div className="flex gap-1 items-center">
+        <div className="flex gap-1 items-center  my-1 rounded-xl bg-richblack-300 p-1 px-1.5">
           <FaRegEye />
           {ratings}
         </div>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import kantara from "../assets/kantara.jpeg";
 export default function Dashboard() {
   return (
     <div>
-      <MovieCard duration="1h 05 mins" ratings="2.3k" image={kantara} />
+      <MovieCard duration="1h 05min" ratings="2.3k" image={kantara} />
     </div>
   );
 }

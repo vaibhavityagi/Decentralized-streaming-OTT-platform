5e53569506bbd04d468f02d9846b0119ddb49676
unknown <jhashivangisingh03@gmail.com>
2024-04-12 19:00:20 +0530

    css of landing images

--- frontend/src/components/Plans.jsx
+++ frontend/src/components/Plans.jsx
@@ -1,4 +1,5 @@
 import Card from "./Card";
+import "../App.css";
 
 export default function Plans() {
   return (
@@ -8,18 +9,20 @@ export default function Plans() {
         Join *** and select from our flexible subscription options tailored to
         suit your viewing preferences. Get ready for non-stop entertainment!
       </p>
-      <Card type="plan" heading="Basic Plan" price={99} time="day">
-        Enjoy an extensive library of movies and shows, featuring a range of
-        content, including recently released titles.
-      </Card>
-      <Card type="plan" heading="Standard Plan" price={199} time="month">
-        Access to a wider selection of movies and shows, including most new
-        releases and exclusive content{" "}
-      </Card>
-      <Card type="plan" heading="Premium Plan" price={1199} time="year">
-        Access to a widest selection of movies and shows, including all new
-        releases and Offline Viewing{" "}
-      </Card>
+      <div className="card-plann">
+        <div><Card type="plan" heading="Basic Plan" price={99} time="day">
+          Enjoy an extensive library of movies and shows, featuring a range of
+          content, including recently released titles.
+        </Card></div>
+        <div><Card type="plan" heading="Standard Plan" price={199} time="month">
+          Access to a wider selection of movies and shows, including most new
+          releases and exclusive content{" "}
+        </Card></div>
+        <div><Card type="plan" heading="Premium Plan" price={1199} time="year">
+          Access to a widest selection of movies and shows, including all new
+          releases and Offline Viewing{" "}
+        </Card></div>
+      </div>
     </div>
   );
 }

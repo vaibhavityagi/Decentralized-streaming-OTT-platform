63d9025cc477b328090229094b6cdb6083fece1e
vaibhavityagi <vaibhavityagi5@gmail.com>
2024-04-12 13:58:15 +0530

    added title component

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,11 @@
+import VideoButton from "./VideoButton";
+
+export default function Title() {
+  return (
+    <div>
+      <h2>The best streaming experience</h2>
+      <p>Now only at ₹99/-</p>
+      <VideoButton text="Start Watching Now" color="blue" border="" />
+    </div>
+  );
+}
--- frontend/src/components/VideoButton.jsx
+++ frontend/src/components/VideoButton.jsx
@@ -1,11 +1,11 @@
 import Button from "./Button";
 import { FaPlay } from "react-icons/fa";
 
-export default function VideoButton() {
+export default function VideoButton({ text, color, border }) {
   return (
     <div>
       <FaPlay />
-      <Button />
+      <Button text={text} color={color} border={border} />
     </div>
   );
 }

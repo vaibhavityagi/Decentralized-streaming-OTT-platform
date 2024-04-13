45c0b7ded99aeb8b5f623290be56a46c806944a5
Ashutosh Mishra <ashuu.pvt@gmail.com>
2024-04-13 11:36:52 +0530

    movies css

--- frontend/src/components/MoviePlay.jsx
+++ frontend/src/components/MoviePlay.jsx
@@ -1,15 +1,28 @@
 import kantaraFull from "../assets/kantaraFullPage.jpg";
+import "../css/movieplay.css";
 import Button from "./Button";
 
 export default function MoviePlay({ name, description }) {
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <Button text="Play Now" color="blue" type="video" />
+    <div className="main-body">
+    <div className="bg-container">
+    <div className="kantara-head">
+    <h2>{name}</h2>
+    </div>
+    <div className="kantara-desc">
+    <p>{description}</p>
+    </div>
+   
+  <div className="kantara-icons">
+  <Button text="Play Now" color="blue" type="video" />
       <Button text="+" color="black" border="" />
       <Button text="👍🏾" color="black" border="" />
       <Button text="🔊" color="black" border="" />
+    
+  </div>
+      
+
+    </div>
     </div>
   );
 }

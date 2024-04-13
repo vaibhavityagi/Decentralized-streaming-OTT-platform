0f45fc254adf8983673b6e45d4129aff328f32c5
unknown <jhashivangisingh03@gmail.com>
2024-04-13 15:24:27 +0530

    don't know

--- frontend/src/components/ManageProfile.jsx
+++ frontend/src/components/ManageProfile.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import ShowPlan from "./ShowPlan"
+import "../css/manageProfile.css";
 
 const ManageProfile = () => {
-  return (
-    <div>
-        <h1>Welcome to your Profile</h1>
+    return (
         <div>
-            <p>Your Plan</p>
-            <ShowPlan ></ShowPlan>
-        </div>
+            <h1>Welcome to your Profile</h1>
+            <div>
+                <p>Your Plan</p>
+                <ShowPlan ></ShowPlan>
+            </div>
 
-        <div>
-            <p>Manage your plan</p>
-        </div>
+            <div>
+                <p>Manage your plan</p>
+            </div>
 
-        <div>
-            <p>Edit Profile</p>
-        </div>
+            <div>
+                <p>Edit Profile</p>
+            </div>
 
-        <div>
-            <p>Payment History</p>
-        </div>
+            <div>
+                <p>Payment History</p>
+            </div>
 
-        <div>
-            <p>Log Out</p>
-        </div>
+            <div>
+                <p>Log Out</p>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default ManageProfile

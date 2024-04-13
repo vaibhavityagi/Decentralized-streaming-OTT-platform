0c36ab4d3ffce7e15a24d02a0dc82b843feca3c1
unknown <jhashivangisingh03@gmail.com>
2024-04-13 18:30:08 +0530

    button alignment

--- frontend/src/components/MoviePlay.jsx
+++ frontend/src/components/MoviePlay.jsx
@@ -26,9 +26,9 @@ export default function MoviePlay({ name, description }) {
         <p>{result.Plot}</p>
         <div className="Movie-icons">
           <Button text="Play Now" color="#00a3ff" type="video" />
-          <Button text={<FaPlus size="1rem"/>} color="#99999973" border="1px solid white" />
-          <Button text={<FaThumbsUp size="1.3rem"/>}  color="#99999973" border="1px solid white" />
-          <Button text={<GiSpeaker size="2rem"/>} color="#99999973" border="1px solid white" />
+          <Button text={<FaPlus size="1rem" />} color="#99999973" border="1px solid white" />
+          <Button text={<FaThumbsUp size="1.3rem" />} color="#99999973" border="1px solid white" />
+          <Button text={<GiSpeaker size="2rem" />} color="#99999973" border="1px solid white" />
         </div>
       </div>
     </div>

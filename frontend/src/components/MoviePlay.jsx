f83c7c22e81efc70bc1bd264216648457c7fb549
vaibhavityagi <vaibhavityagi5@gmail.com>
2024-04-13 11:41:45 +0530

    added movie api

--- frontend/src/components/MoviePlay.jsx
+++ frontend/src/components/MoviePlay.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import kantaraFull from "../assets/kantaraFullPage.jpg";
 import Button from "./Button";
+import axios from "axios";
 
 export default function MoviePlay({ name, description }) {
+  const [result, setResult] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        "https://www.omdbapi.com/?i=tt3896198&apikey=fa3ff5d2"
+      );
+      console.log(res.data);
+      setResult(res.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>{result.Title}</h2>
       <p>{description}</p>
       <Button text="Play Now" color="blue" type="video" />
       <Button text="+" color="black" border="" />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,27 +7,35 @@ export default function Navbar({ isLoggedIn }) {
   return (
     <div className="navbar">
       <div className="navbar1">
-        <a><div className="u">Movies</div></a>
-        <a><div className="u">TV Shows</div></a>
-        <a><div className="u">Documentaries</div></a>
+        <a>
+          <div className="u">Movies</div>
+        </a>
+        <a>
+          <div className="u">TV Shows</div>
+        </a>
+        <a>
+          <div className="u">Documentaries</div>
+        </a>
       </div>
       <div className="websiteName">"Website Name"</div>
       <div className="nav-profile-s">
-        <div className="inputtt">{isLoggedIn ? <input type="text" /> : null}</div>
-        {
-          isLoggedIn ? (
-            <div>Profile</div>
-          ) : (
-            <div
-              onClick={() => {
-                navigate("/login");
-              }}
-            >
-              <a><div className="u">Login</div></a>
-            </div>
-          )
-        }
+        <div className="inputtt">
+          {isLoggedIn ? <input type="text" /> : null}
+        </div>
+        {isLoggedIn ? (
+          <div>Profile</div>
+        ) : (
+          <div
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            <a>
+              <div className="u">Login/Signup</div>
+            </a>
+          </div>
+        )}
       </div>
-    </div >
+    </div>
   );
 }
